77561b0b10b89d93ffd61666bb2f6a9d6643964a
KEINance <keinance2@gmail.com>
2023-05-06 23:00:11 -0400

    added functions and testing error

--- js/script.js
+++ js/script.js
@@ -1,39 +1,58 @@
-// Enables random password generate 
-// multiple screen sizes.
-// The password can include special characters. 
-// (https://www.owasp.org/index.php/Password_special_characters) from the OWASP Foundation.
 // Assignment Code
-//upper, lower, numbers and special
-//ALERTS!!!!! for the window pop ups
-const generateBtn = document.querySelector("#generate");
-const generateText = document.querySelector("#password");
-const result = document.querySelector("#result");
-const passLength = document.querySelector("#length");
-const includeUpperCase = document.querySelector("#uppercase");
-const includeLowerCase = document.querySelector("#lowercase");
-const includeNumbers = document.querySelector("#numbers");
-const includeSymbols = document.querySelector("#symbols");
+var generateBtn = document.querySelector("#generate");
 
-passLength.addEventListener("change", (event) => {
-    passLengthResult.innerText = event.target.value;
-});
+
+lowerCaseChars = "abcdefghijklmnopqrstuvwxyz";
+upperCaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+numberChars = "1234567890";
+specialChars = "!@#$%^&**(){}[]<>/?";
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+
+// function length ()
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+function writePassword() {
+    var length = length.value;
+    var hasLower = lowerCaseChars.checked;
+    var hasUpper = upperCaseChars.checked;
+    var hasNumber = numberChars.checked;
+    var hasSymbols = specialChars.checked;
+
+    generateBtn.innerText = generatePassword(
+        hasLower, 
+        hasUpper, 
+        hasNumber, 
+        hasSymbols);
+};
+
+generateBtn.addEventListener("click", writePassword);{
 
 function getRandomLower() {
+    var lowerCaseChars = "abcdefghijklmnopqrstuvwxyz";
     return String.fromCharCode(math.Floor(Math.random() *26)+ 97);
 }
 
 function getRandomUpper() {
+    var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     return String.fromCharCode(math.Floor(Math.random() *26)+ 65);
 }
 
 function getRandomNumber() {
+    var numbers = "1234567890";
     return String.fromCharCode(math.Floor(Math.random() *10)+ 48);
 }
 
 function getRandomSymbol() {
-    const symbols = "!@#$%^&*(){}[]<>"
+    var symbols = "!@#$%^&**(){}[]<>/?";
     return[math.Floor(math.Random() * symbols.length)]
      String.fromCharCode(math.Floor(Math.random() *10)+ 48);
 }
- console.log(String.fromCharCode(math.Floor(Math.random() *26)+ 97));
-
+}
