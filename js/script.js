dd870310330c92fda30fb40d878f847c162f85eb
KEINance <keinance2@gmail.com>
2023-05-09 21:26:03 -0400

    var name change

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 // Assignment Code
 // Write password to the #password input
-//result
-var generateBtn = document.querySelector("#generate");     //HERE
+
+var generateBtn = document.querySelector("#generate");   
 var password = document.querySelector("#password");
 
-// add promtps and confirms
+// add promtps and confirms for user to make character choices
 alert("Please, choose password options!");
 
 confirm(
@@ -26,18 +26,22 @@ confirm(
 prompt(
     "What character length would be prefered between 8-128?", 
 );
+
+// choose chacters at random func
 function generatePassword(){
     return String.fromCharCode(Math.floor(Math.random() * 77) + 34);
 };
 
+// allow password to be generated and returned
 function passwordGenerator(num) {
-    var password = '';
+    var result = '';
     for (var i = 0; i < num; i++) {
-      password += getPasswordCharacter();
+      result += generatePassword();
     }
-    return password;
+    return result;
   };
 
+  // Write password to the #password input
 function writePassword(num) {                         
     var length = generatePassword();
     var passwordText = document.querySelector("#password");
@@ -46,4 +50,4 @@ function writePassword(num) {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword) 
+generateBtn.addEventListener("click", writePassword);
