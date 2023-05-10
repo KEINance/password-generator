26e9b2e182748c8a49aa6746da7ea475c3f02fb5
KEINance <keinance2@gmail.com>
2023-05-09 20:50:37 -0400

    trimming excess code and fixing syntax errors

--- js/script.js
+++ js/script.js
@@ -3,99 +3,9 @@
 //result
 var generateBtn = document.querySelector("#generate");     //HERE
 var password = document.querySelector("#password");
-var lowerCaseChars = "abcdefghijklmnopqrstuvwxyz";
-var upperCaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numberChars = "1234567890";
-var symbolChars = "!@#$%^&**(){}[]<>/?";
-var length = "length";
-var result = "result"
-
-
-var random = {
-    "lower" : getRandomLower,
-    "upper" : getRandomUpper,
-    "number" : getRandomNumber,
-    "symbol" : getRandomSymbol,
-}
-
-//define length
-(function() {
-    ("password").html(length.val());
-    length.on("change", function(){
-        ("password").html((this).val())
-    });
-
-})
-
-// need to create function for genpass
-// add promtps and confirms for user choice in characters for password
-//validate input 
-//gen password off criteria
-generateBtn("click", function){
-    var length = length.valueOf;
-    var lowerCaseChars = lowerCaseChars.checked;
-    var upperCaseChars = upperCaseChars.checked;
-    var numberChars = numberChars.checked;
-    va
-    symbolChars 
-    symbolChars.checked;
-
-generateBtn.innerText = generatePassword(
-        hasLower, 
-        hasUpper, 
-        hasNumber, 
-        hasSymbols,
-        length
-        );
-};
-
-function generatePassword(hasLower, hasUpper, hassymbol, hasNumber) {
-    var generatePasswordStr = "";
-    var typeArr = {
-        "lower" : lower,
-        "upper" : upper,
-        "number" : number,
-        "symbol" : symbol,
-    }
-};
-
-for(let i = 0; i < length; i++) {
-        each(typesArr, function(key, value)) {
-            if(value != false){
-                var functionname = key;
-                generatePasswordSTr += random{functionName}();
-            }
-        }
-      var finalPassword = generatePasswordStr.slice(0, length);
-      return finalPassword;
-        
-    };
-
-function getRandomLower() {
-    return String.fromCharCode(Math.floor(Math.random() *26)+ 97);
-}
-function getRandomUpper() {
-    return String.fromCharCode(Math.floor(Math.random() *26)+ 65);
-}
-function getRandomNumber() {
-    return String.fromCharCode(Math.floor(Math.random() *10)+ 48);
-}
-function getRandomSymbol() {
-    var symbolChars = "!@#$%^&**(){}[]<>/?";
-    // return[math.Floor(Math.random() * symbols.length)]
-     String.fromCharCode[Math.floor(Math.random)() = symbols.length];
-}
-
-function writePassword() {                          //HERE
-    var length = generatePassword();
-    var passwordText = document.querySelector("#password");
-    document.querySelector("password").textContentext = password;    //blue is extra
-}
-
-
 
 // add promtps and confirms
-alert ("Please, choose password options!");
+alert("Please, choose password options!");
 
 confirm(
     "Would you like upper case characters?", 
@@ -116,7 +26,24 @@ confirm(
 prompt(
     "What character length would be prefered between 8-128?", 
 );
+function generatePassword(){
+    return String.fromCharCode(Math.floor(Math.random() * 77) + 34);
+};
 
+function passwordGenerator(num) {
+    var password = '';
+    for (var i = 0; i < num; i++) {
+      password += getPasswordCharacter();
+    }
+    return password;
+  };
+
+function writePassword(num) {                         
+    var length = generatePassword();
+    var passwordText = document.querySelector("#password");
+    
+    passwordText.value = password;
+}
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);        // HERE
+generateBtn.addEventListener("click", writePassword) 
